1543b6ec1c0adbd7ef4aca3b595d4cf7316dfd28
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2019-07-28 16:21:44 +1000

    Changed folder structure

--- flask/imgconverter.py
+++
@@ -1,2 +0,0 @@
-def path_to_image_html(path):
-    return '<img src="'+ path + '" width="60" >'
---
+++ flightscraper.py
@@ -0,0 +1,101 @@
+def flightscraper():
+
+    from datetime import datetime
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from bs4 import BeautifulSoup
+    import numpy as np
+    import pandas as pd
+
+    # Initialise instance of Chrome driver
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")
+    ##System.setProperty("webdriver.chrome.driver", "C:\\Users\\michael\\Downloads\\chromedriver_win32.exe");
+    ##driver = webdriver.Chrome(executable_path=os.path.abspath("chromedriver.exe")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    # Define query strings for url
+    today = datetime.today().strftime('%Y-%m-%d')
+    flight_types  = ["arrival", "departure"]
+    terminal_types = ["international", "domestic"]
+
+    # Initialise empty lists
+    journies = []
+    types = []
+    stopovers = []
+    airlines = []
+    airline_logos = []
+    flight_numbers = []
+    statuses = []
+    scheduled_times = []
+    estimated_times = []
+
+    # Loop through each combination of (domestic/international) x (arrival/departure)
+    for flight_type in flight_types:
+        for terminal_type in terminal_types:
+            
+            url = "https://www.sydneyairport.com.au/flights/?query=&flightType=" + flight_type + "&terminalType=" + terminal_type + "&date=" + today + "&sortColumn=scheduled_time&ascending=true&showAll=true"
+            driver.get(url)
+            
+            html_soup = BeautifulSoup(driver.page_source, "html.parser")
+            flight_containers = html_soup.find_all("div", attrs={"class": "flight-card"})[2:]
+            
+            # Loop through all containers (one for each flight) to extract and store info
+            for container in flight_containers:
+                
+                # International / domestic and Arrival / departure
+                if (terminal_type == "international") & (flight_type == "arrival"):
+                    types.append("static/images/international_arrival.png")
+                elif (terminal_type == "international") & (flight_type == "departure"):
+                    types.append("static/images/international_departure.png")
+                elif (terminal_type == "domestic") & (flight_type == "arrival"):
+                    types.append("static/images/domestic_arrival.png")
+                else:
+                    types.append("static/images/domestic_departure.png")
+
+                # Origin / Destinations
+                journies.append(container.find("div", attrs={"class": "destination-name"}).text)
+
+                # Stopovers (if any)
+                if container.find("div", attrs={"class": "city-via"}) is not None:
+                    stopovers.append(container.find("div", attrs={"class": "city-via"}).text)
+                else:
+                    stopovers.append("-")
+
+                # Airlines
+                airlines.append(container.find("span", attrs={"class": "with-image"}).text)
+
+                # Airline logos
+                airline_logos.append("https://www.sydneyairport.com.au" + container.img['src'])
+
+                # Flight numbers
+                flight_numbers.append(container.find("div", attrs={"class": "heading-medium"}).text)
+
+                # Statuses
+                statuses.append(container.find("div", attrs={"class": "status"}).text)
+
+                # Scheduled times
+                scheduled_times.append(container.find("div", attrs={"class": "large-scheduled-time"}).text)
+
+                # Estimated times
+                estimated_times.append(container.find("div", attrs={"class": "estimated-time"}).text)
+        
+        
+    # Create dataframe from lists
+    flights = pd.DataFrame({'Journey': journies,
+                            'Type': types,
+                            'Stopover': stopovers,
+                            'Airline': airlines,
+                            'Logo': airline_logos,
+                            'Flight number': flight_numbers,
+                            'Status': statuses,
+                            'Scheduled time': scheduled_times,
+                            'Estimated time': estimated_times})
+    # Reorder columns
+    flights = flights[['Type', 'Journey', 'Stopover', 'Airline', 'Logo', 'Flight number', 'Status', 'Scheduled time', 'Estimated time']]
+    
+    # Order flights by scheduled time
+    flights.sort_values(by=['Scheduled time'], inplace=True)
+    flights.reset_index(drop=True, inplace=True)
+    
+    return flights
