6e6a652f9bd238b0812c7589c11416849f9ac2f3
Naomi Stuart <nstu778@aucklanduni.ac.nz>
2019-08-10 13:04:41 +1000

    added scheduling tasks

---
+++ flightscraper2.py
@@ -0,0 +1,92 @@
+def flightscraper2(terminal, journey):
+
+    from datetime import datetime
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from bs4 import BeautifulSoup
+    import numpy as np
+    import pandas as pd
+    import os
+    import time
+
+    # Initialise instance of Chrome driver
+    chrome_options = webdriver.ChromeOptions()
+    if (os.environ.get("GOOGLE_CHROME_BIN") is None) | (os.environ.get("CHROMEDRIVER_PATH") is None):
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+    else:
+       chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+       chrome_options.add_argument("--headless")
+       chrome_options.add_argument("--disable-dev-shm-usage")
+       chrome_options.add_argument("--no-sandbox")
+       driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
+    # Define query strings for url
+    today = datetime.today().strftime('%Y-%m-%d')
+
+    # Initialise empty lists
+    journies = []
+    types = []
+    stopovers = []
+    airlines = []
+    airline_logos = []
+    flight_numbers = []
+    statuses = []
+    scheduled_times = []
+    estimated_times = []
+
+    # Loop through each combination of (domestic/international) x (arrival/departure)
+    url = "https://www.sydneyairport.com.au/flights/?query=&flightType=" + journey + "&terminalType=" + terminal + "&date=" + today + "&sortColumn=scheduled_time&ascending=true&showAll=true"
+    print(url) # logs
+    driver.get(url)
+    
+    html_soup = BeautifulSoup(driver.page_source, "html.parser")
+    flight_containers = html_soup.find_all("div", attrs={"class": "flight-card"})[2:]
+    
+    # Loop through all containers (one for each flight) to extract and store info
+    for container in flight_containers:
+        
+        # International / domestic and Arrival / departure
+        types.append('static/images/{}_{}.png'.format(terminal, journey))
+
+        # Origin / Destinations
+        journies.append(container.find("div", attrs={"class": "destination-name"}).text)
+
+        # Stopovers (if any)
+        if container.find("div", attrs={"class": "city-via"}) is not None:
+            stopovers.append(container.find("div", attrs={"class": "city-via"}).text)
+        else:
+            stopovers.append("-")
+
+        # Airlines
+        airlines.append(container.find("span", attrs={"class": "with-image"}).text)
+
+        # Airline logos
+        airline_logos.append("https://www.sydneyairport.com.au" + container.img['src'])
+
+        # Flight numbers
+        flight_numbers.append(container.find("div", attrs={"class": "heading-medium"}).text)
+
+        # Statuses
+        statuses.append(container.find("div", attrs={"class": "status"}).text)
+
+        # Scheduled times
+        scheduled_times.append(container.find("div", attrs={"class": "large-scheduled-time"}).text[0:5])
+
+        # Estimated times
+        estimated_times.append(container.find("div", attrs={"class": "estimated-time"}).text[0:5])
+    
+    # Create dictionary of flights
+    flights = {
+        'Type': types,
+        'Journey': journies,
+        'Stopover': stopovers,
+        'Airline': airlines,
+        'Logo': airline_logos,
+        'Flight number': flight_numbers,
+        'Status': statuses,
+        'Scheduled time': scheduled_times,
+        'Estimated time': estimated_times 
+    }
+   
+    return flights
--- main.py
+++ main.py
@@ -2,19 +2,25 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_heroku import Heroku
 import pandas as pd
-from flightscraper import flightscraper
+from flightscraper2 import flightscraper2
 from imgconverter import path_to_image_html
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import atexit
+import time
+from datetime import datetime
 
-#basedir = os.path.abspath(os.path.dirname(__file__))
-
+# Set up Flask app
 app = Flask(__name__)
-#DATABASE URL FOR HEROKU POSTGRES CAN BE FOUND UNDER CONFIG VARS
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# Uncomment below lines for local testing using SQLite database
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 
+# Define Flight table schema
 class Flight(db.Model):
     __tablename__ = 'flights'
 
@@ -28,6 +34,8 @@ class Flight(db.Model):
     status = db.Column(db.String())
     scheduled_time = db.Column(db.String())
     estimated_time = db.Column(db.String())
+    #
+    time_modified = db.Column(db.String())
 
     def __init__(self, flight_type, journey, stopover, airline, airline_logo, flight_number, status, scheduled_time, estimated_time):
         self.flight_type = flight_type
@@ -39,6 +47,8 @@ class Flight(db.Model):
         self.status = status
         self.scheduled_time = scheduled_time
         self.estimated_time = estimated_time
+        #
+        self.time_modified = time_modified
 
     def __repr__(self):
         return '<flight number {}>'.format(self.flight_number)
@@ -54,10 +64,61 @@ class Flight(db.Model):
             'flight_number': self.flight_number,
             'status': self.status,
             'scheduled_time': self.scheduled_time,
-            'estimated_time': self.estimated_time
+            'estimated_time': self.estimated_time,
+            #
+            'time_modified': self.time_modified
         }
 
+# Function to refresh flight data - this will be run on cron schedule
+def refresh_flights(terminal, journey):
+    # Delete rows in table corresponding to terminal and journey type
+    flight_type_str = 'static/images/{}_{}.png'.format(terminal, journey)
+    delete_rows = Flight.delete().where(Flight.flight_type==flight_type_str)
+    db.session.execute()
+    db.session.commit()
 
+    # Get new data
+    flights = flightscraper2(terminal=terminal, journey=journey)
+
+    # Save new data in database
+    for i in range(0, len(flights["Type"])):
+        try:
+            flight = Flight(
+                flight_type=flights["Type"][i],
+                journey=flights["Journey"][i],
+                stopover=flights["Stopover"][i],
+                airline=flights["Airline"][i],
+                airline_logo=flights["Logo"][i],
+                flight_number=flights["Flight number"][i],
+                status=flights["Status"][i],
+                scheduled_time=flights["Scheduled time"][i],
+                estimated_time=flights["Estimated time"][i],
+                time_modified=datetime.now()
+            )
+
+            db.session.add(flight)
+            db.session.commit()
+            print("Flight added, flight number={}".format(flight.flight_number))
+        
+        except Exception as e:
+            print(str(e))
+
+
+# Set up cron schedules to refresh data
+schedules = ["0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55",
+             "1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56",
+             "2, 7, 12, 17, 22, 27, 32, 37, 42, 47, 52, 57",
+             "3, 8, 13, 18, 23, 28, 33, 38, 43, 48, 53, 58"]
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(refresh_flights(terminal="international", journey="arrival"), "cron", minute=schedules[0])
+scheduler.add_job(refresh_flights(terminal="international", journey="departure"), "cron", minute=schedules[1])
+scheduler.add_job(refresh_flights(terminal="domestic", journey="arrival"), "cron", minute=schedules[2])
+scheduler.add_job(refresh_flights(terminal="domestic", journey="departure"), "cron", minute=schedules[3])
+
+atexit.register(lambda: scheduler.shutdown())
+
+# Define app routes
 @app.route("/")
 def loading():
     return render_template("loading.html")
@@ -94,12 +155,6 @@ def refresh_data():
     return "END OF DATA LOAD"
 
 @app.route("/results")
-def results():
-    flights = flightscraper(returnDataFrame=True)
-    pd.options.display.max_colwidth = 200
-    return render_template("flights.html", tables=[flights.to_html(index=False, classes="flights", escape=False, formatters=dict(Type=path_to_image_html, Logo=path_to_image_html))], titles=['Sydney flights'])
-
-@app.route("/newresults")
 def newresults():
     flights = pd.read_sql(Flight.query.statement, db.session.bind)
     flights.rename(columns={
@@ -111,7 +166,8 @@ def newresults():
         "flight_number": "Flight number",
         "status": "Status",
         "scheduled_time": "Scheduled time",
-        "estimated_time": "Estimated time"}, inplace=True)
+        "estimated_time": "Estimated time",
+        "time_modified": "Time Modified"}, inplace=True)
     flights.sort_values(by=['Scheduled time'], inplace=True)
     flights.drop(columns=["id"], inplace=True)
 
